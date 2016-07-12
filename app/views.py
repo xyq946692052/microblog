c4d8918d224e59f382d54b68dde34ab1c95d87f2
xyq <xyq2312@sina.com>
2016-07-13 00:32:37 +0800

    learn about the jinja2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello flask!'
+    user={'nickname':'Kevin Xie'}  #fade user
+    posts=[    #fade array of posts
+              {'author':{'nickname':'John'},
+                'body':'beautiful day in Portland!'
+              },
+              {
+                'author':{'nickname':'Anne'},
+                'body':'The Avengers movie was cool!'
+              } 
+
+          ]
+    return render_template('index.html',title='Home',user=user,posts=posts)
+
+
