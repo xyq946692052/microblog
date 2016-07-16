6a9dda2bd0aaa590e6d852f3087c960fbee01f15
xyq <xyq2312@sina.com>
2016-07-16 17:06:59 +0800

    modify

--- config.py
+++ config.py
@@ -1,21 +1,15 @@
 import os
-
-
-CSRF_ENABLED=True          
-SECRET_KEY='you-will-never-guess'
-
-OPENID_PROVIDERS=[
-    {'name':'Google','url':'https://www.google.com/accounts/o9/id'},
-    {'name':'Baidu','url':'http://www.baidu.com'},
-    {'name':'Taobao','url':'http://www.taobao.com'},
-    {'name':'Sohu','url':'http://www.sohu.com'},
-    {'name':'CSDN','url':'http://www.csdn.net'},
-]
-
-#=====================
-#database
-#=====================
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+    
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
